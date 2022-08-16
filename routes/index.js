f78dec7dd20c320f4e98a53c7a5d5d5c059e12cd
Donald_ga <313771069@qq.com>
2022-08-16 10:37:35 +0800

    stability表插入测试数据&数据库查询测试

--- routes/index.js
+++ routes/index.js
@@ -8,11 +8,11 @@ router.get('/', function(req, res, next) {
   // 对数据库某个特定的表进行查询
   connection.query('select * from stability', (err, users) => {
     if(err) {
-      res.render('index', { title: 'query error' });
-      // res.send('query error')
+      // res.render('index', { title: 'query error' });
+      res.send('query error')
     } else {
-      res.render('index', { title: users });
-      // res.send(users)
+      // res.render('index', { title: users });
+      res.send(users)
     }
   })
   // res.render('index', { title: 'Express' });
