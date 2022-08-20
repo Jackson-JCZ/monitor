322acccd5fcc33dd15a159ec311c6edbf9ca1879
Donald_ga <313771069@qq.com>
2022-08-20 15:30:50 +0800

    使用cors包开启跨域允许

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ var usersRouter = require('./routes/users');
 var home_data = require('./routes/home_data')
 var sdk_post = require('./routes/sdk_post')
 var app = express();
+const cors = require('cors')
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -20,6 +22,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
